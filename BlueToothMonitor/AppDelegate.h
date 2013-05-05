30b7a8062681b309e3c3b0b917eecf5afe3a2889
Jase Bell <jasebell@gmail.com>
2013-05-05 07:53:32 +0100

    Initial Commit

---
+++ BlueToothMonitor/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  BlueToothMonitor
+//
+//  Created by Jason Bell on 05/05/2013.
+//  Copyright (c) 2013 Jason Bell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ BlueToothMonitor/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BlueToothMonitor
+//
+//  Created by Jason Bell on 05/05/2013.
+//  Copyright (c) 2013 Jason Bell. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
